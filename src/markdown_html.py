ba6289b44d9c8888ed65d919f836f82f6ac8907c
Burush0 <burush02@gmail.com>
2025-03-02 00:18:51 +0100

    Added unit tests for markdown to html function, refactored helper functions

--- src/markdown_html.py
+++ src/markdown_html.py
@@ -1,7 +1,7 @@
 from htmlnode import HTMLNode, ParentNode, LeafNode
 from textnode import text_node_to_html_node
 from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
-from inline_markdown import text_to_textnodes
+from inline_markdown import text_to_textnodes, TextNode, TextType
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
@@ -15,25 +15,24 @@ def markdown_to_html_node(markdown):
 # 8 helper functions (poggers)
 def block_to_html_node(block):
     block_type = block_to_block_type(block)
-        match block_type:
-            case BlockType.PARAGRAPH:
-                children.append(block_to_paragraph(block))
-            case BlockType.HEADING:
-                children.append(block_to_heading(block))
-            case BlockType.QUOTE:
-                children.append(block_to_quote(block))
-            case BlockType.ULIST:
-                children.append(block_to_ulist(block))
-            case BlockType.OLIST:
-                children.append(block_to_olist(block))
-            case BlockType.CODE:
-                children.append(block_to_code(block))
-            case _:
-                raise ValueError(f"invalid block type: {block_type}")
+    match block_type:
+        case BlockType.PARAGRAPH:
+            return block_to_paragraph(block)
+        case BlockType.HEADING:
+            return block_to_heading(block)
+        case BlockType.QUOTE:
+            return block_to_quote(block)
+        case BlockType.ULIST:
+            return block_to_ulist(block)
+        case BlockType.OLIST:
+            return block_to_olist(block)
+        case BlockType.CODE:
+            return block_to_code(block)
+        case _:
+            raise ValueError(f"invalid block type: {block_type}")
 
 def text_to_children(text):
     text_nodes = text_to_textnodes(text)
-    print(text_nodes)
     html_nodes = []
     for node in text_nodes:
         html_nodes.append(text_node_to_html_node(node))
@@ -42,8 +41,7 @@ def text_to_children(text):
 def block_to_paragraph(block):
     block = block.replace("\n", " ")
     children = text_to_children(block)
-    parent = ParentNode("p", children)
-    return parent
+    return ParentNode("p", children)
 
 def block_to_heading(block):
     #lines = block.split("\n")
@@ -64,9 +62,12 @@ def block_to_heading(block):
     # return nodes
 
 def block_to_quote(block):
-    stripped_block = block.lstrip(">")
-    stripped_block = stripped_block.lstrip(" ")
-    children = text_to_children(stripped_block)
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
     return ParentNode("blockquote", children)
 
 def block_to_olist(block):
@@ -92,52 +93,13 @@ def block_to_ulist(block):
     return ParentNode("ul", children)
 
 def block_to_code(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block")
     block = block.strip("```")
-    child = [LeafNode("code", block)]
-    return ParentNode("pre", child)
+    block = block.lstrip("\n")
+    raw = TextNode(block, TextType.TEXT)    
+    child = text_node_to_html_node(raw)
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
 
 # TODO add images and links as well
-
-# paragraphs
-md = """
-This is **bolded** paragraph
-text in a p
-tag here
-
-# This is a heading 1
-
-## This is a heading 2
-
-### This is a heading 3
-
-> This is a quote with a **bold** and _italic_ words
-
-1. list entry 1
-2. list entry 2
-3. list entry 3
-
-- bananas
-- cherries
-- more bananas, _why not_
-
-This is another paragraph with _italic_ text and `code` here
-
-"""
-node = markdown_to_html_node(md)
-html = node.to_html()
-result = "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>"
-print(html)
-print(result)
-
-# code block
-md = """
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
-"""
-node = markdown_to_html_node(md)
-html = node.to_html()
-result = "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>"
-print(html)
-print(result)
